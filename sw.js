d77f31de40e0221e3a5c4e530185f8d970811eed
l3ickey <bickey_t0mat0@outlook.jp>
2026-09-14 00:13:59 +0900

    Release v1.4.20 with larger 32px task monsters

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 // Change the version whenever any shipped asset changes.
-const CACHE='hitotsu-v1:'+self.registration.scope+':1.4.18:578e3c1d7dba';
+const CACHE='hitotsu-v1:'+self.registration.scope+':1.4.20:af4c0309d74a';
 const PREFIX='hitotsu-v1:'+self.registration.scope+':';
 const ASSETS=['./','./index.html','./firebase-sdk.js','./about.html','./privacy.html','./characters.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
