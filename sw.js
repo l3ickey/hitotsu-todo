87d06e8a21737ee1e60905378ac7737133693b9a
l3ickey <bickey_t0mat0@outlook.jp>
2026-09-14 13:24:49 +0900

    Release v1.4.23 with battle sounds and preserved child expansion

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 // Change the version whenever any shipped asset changes.
-const CACHE='hitotsu-v1:'+self.registration.scope+':1.4.20:af4c0309d74a';
+const CACHE='hitotsu-v1:'+self.registration.scope+':1.4.23:a6c8fd9ffe15';
 const PREFIX='hitotsu-v1:'+self.registration.scope+':';
 const ASSETS=['./','./index.html','./firebase-sdk.js','./about.html','./privacy.html','./characters.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
