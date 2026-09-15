4b8fa10bb696b354b290990ca95da1f1312e471f
l3ickey <bickey_t0mat0@outlook.jp>
2026-09-15 15:06:28 +0900

    Release v1.5.2 fixing enemy flicker between adventure loops

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 // Change the version whenever any shipped asset changes.
-const CACHE='hitotsu-v1:'+self.registration.scope+':1.5.1:a01f11209fef';
+const CACHE='hitotsu-v1:'+self.registration.scope+':1.5.2:cf1fdeee779b';
 const PREFIX='hitotsu-v1:'+self.registration.scope+':';
 const ASSETS=['./','./index.html','./firebase-sdk.js','./about.html','./privacy.html','./characters.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
