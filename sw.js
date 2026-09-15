ddfd98c7cf7948868bde1a8735c9c97142336359
l3ickey <bickey_t0mat0@outlook.jp>
2026-09-15 14:55:13 +0900

    Release v1.5.1 fixing iPhone adventure safe-area background

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 // Change the version whenever any shipped asset changes.
-const CACHE='hitotsu-v1:'+self.registration.scope+':1.5.0:34bda7ba3b2c';
+const CACHE='hitotsu-v1:'+self.registration.scope+':1.5.1:a01f11209fef';
 const PREFIX='hitotsu-v1:'+self.registration.scope+':';
 const ASSETS=['./','./index.html','./firebase-sdk.js','./about.html','./privacy.html','./characters.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
