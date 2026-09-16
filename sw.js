f7c8fe556d07f75c342425d87ce4fb5e57b96598
l3ickey <bickey_t0mat0@outlook.jp>
2026-09-17 01:16:17 +0900

    Release 1.7.3: refine slash shape, colors and hit alignment

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 // Change the version whenever any shipped asset changes.
-const CACHE='hitotsu-v1:'+self.registration.scope+':1.7.2:2a85f6712579';
+const CACHE='hitotsu-v1:'+self.registration.scope+':1.7.3:9bd7e0e05755';
 const PREFIX='hitotsu-v1:'+self.registration.scope+':';
 const ASSETS=['./','./index.html','./firebase-sdk.js','./about.html','./privacy.html','./characters.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
