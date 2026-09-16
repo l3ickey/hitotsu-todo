f3168e63d92d2c9b64d5af420952e8ed622e8470
l3ickey <bickey_t0mat0@outlook.jp>
2026-09-17 04:46:02 +0900

    Release 1.8.0: guided onboarding and sound enabled by default

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 // Change the version whenever any shipped asset changes.
-const CACHE='hitotsu-v1:'+self.registration.scope+':1.7.3:9bd7e0e05755';
+const CACHE='hitotsu-v1:'+self.registration.scope+':1.8.0:79f3bf28a70a';
 const PREFIX='hitotsu-v1:'+self.registration.scope+':';
 const ASSETS=['./','./index.html','./firebase-sdk.js','./about.html','./privacy.html','./characters.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
