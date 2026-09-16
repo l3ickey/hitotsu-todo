297c9ed57bd277d94207e76dcb12b1e038e8c73f
l3ickey <bickey_t0mat0@outlook.jp>
2026-09-17 00:43:28 +0900

    Release 1.7.2: unlock all characters over 100 adventure days

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 // Change the version whenever any shipped asset changes.
-const CACHE='hitotsu-v1:'+self.registration.scope+':1.7.1:6adf32fc7a89';
+const CACHE='hitotsu-v1:'+self.registration.scope+':1.7.2:2a85f6712579';
 const PREFIX='hitotsu-v1:'+self.registration.scope+':';
 const ASSETS=['./','./index.html','./firebase-sdk.js','./about.html','./privacy.html','./characters.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
