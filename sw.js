5abcd931b8f396d9ff9c406ac60d24ce41407175
l3ickey <bickey_t0mat0@outlook.jp>
2026-09-20 10:56:13 +0900

    Release 1.8.3: fix child task deadline priority comparison

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 // Change the version whenever any shipped asset changes.
-const CACHE='hitotsu-v1:'+self.registration.scope+':1.8.2:c4cb6b5adea0';
+const CACHE='hitotsu-v1:'+self.registration.scope+':1.8.3:4ff7b74d37fa';
 const PREFIX='hitotsu-v1:'+self.registration.scope+':';
 const ASSETS=['./','./index.html','./firebase-sdk.js','./about.html','./privacy.html','./characters.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
