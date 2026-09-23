8bb252e2f33591499503996a70867fae8036e1b4
l3ickey <bickey_t0mat0@outlook.jp>
2026-09-23 17:53:06 +0900

    Release 1.9.4: reorder categories and enable touch subtask dragging

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 // Change the version whenever any shipped asset changes.
-const CACHE='hitotsu-v1:'+self.registration.scope+':1.9.3:45fa81486a9b';
+const CACHE='hitotsu-v1:'+self.registration.scope+':1.9.4:f49cf105f90d';
 const PREFIX='hitotsu-v1:'+self.registration.scope+':';
 const ASSETS=['./','./index.html','./firebase-sdk.js','./about.html','./privacy.html','./characters.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
