0a6bd5c3eff181e6d334fbe0b34f1c49e4e5af1c
l3ickey <bickey_t0mat0@outlook.jp>
2026-09-23 10:28:19 +0900

    Release 1.9.0: add task categories with guarded category deletion

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 // Change the version whenever any shipped asset changes.
-const CACHE='hitotsu-v1:'+self.registration.scope+':1.8.4:da69ac0e3ec1';
+const CACHE='hitotsu-v1:'+self.registration.scope+':1.9.0:3e828c48ae2c';
 const PREFIX='hitotsu-v1:'+self.registration.scope+':';
 const ASSETS=['./','./index.html','./firebase-sdk.js','./about.html','./privacy.html','./characters.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
